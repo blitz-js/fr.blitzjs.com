549b7b719d0f65dd011abd801ca2cc2065bf0346
Arthur Joly <Mangiang@users.noreply.github.com>
2021-12-30 15:57:52 -0500

    feat: Add Showcase page translation (#13)

--- app/pages/showcase.js
+++ app/pages/showcase.js
@@ -36,7 +36,7 @@ const ShowcasePage = () => {
         <div className="space-y-6">
           <h1 className="font-primary text-3xl lg:text4xl xl:text-5xl font-semibold">Showcase</h1>
           <p className="font-secondary text-lg text-gray-600 dark:text-gray-300">
-            Here are some beautiful websites built with Blitz.js
+            Voici quelques sites web cr&eacute;&eacute;s gr&acirc;ce &agrave; Blitz.js
           </p>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
@@ -45,14 +45,14 @@ const ShowcasePage = () => {
           })}
         </div>
         <div className="font-secondary text-base text-gray-600 dark:text-gray-300">
-          Want to submit your website?{" "}
+          Si vous souhaitez ajouter votre site web{" "}
           <a
             href="https://github.com/blitz-js/blitzjs.com/edit/main/app/core/utils/showcaseList.js"
             target="_blank"
             rel="noreferrer"
             className="text-purple-light dark:text-purple-extralight font-medium dark:font-bold no-underline dark:underline hover:underline"
           >
-            Here&apos;s a way to do that
+            Voici comment proc&eacute;der
           </a>
         </div>
       </main>
